5d438b6b69f7ec64b513d5c5f385632453138975
Szymon Wygnanski <s@finalclass.net>
2016-12-18 20:09:15 +0100

    -

--- .bin/fc-variables.sh
+++ .bin/fc-variables.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
+# export EID=18a24aad1e3aad246b9e19626000446b # old2
+export EID=446d828df21832cba13800a0d7012f88 # test 333
 # export EID=3d8eb6f2736a9ddb3e4df185a60018ae # OLD
 # export EID=ef1b0e162f5e3353155febbbf6000ba9 # Ev
-export EID=ebd51b42c91476a76ce07c87c5000e56 # A
+# export EID=ebd51b42c91476a76ce07c87c5000e56 # A
 export DS="http://localhost"
 export AUTH_DB="http://admin:rm@localhost"
 export WEBAPP_ENABLED=true
