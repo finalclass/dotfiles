3568e5dc8f7c0c03d5e3dd5fd9612defd57813a9
Szymon Wygnanski <s@finalclass.net>
2017-05-11 08:43:37 +0200

    -

---
+++ .bin/fc-deploy-backstage-to-cloudnode.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+
+
+while [[ $# -gt 0 ]]; do
+    case $1 in
+        -b|--build)
+            BUILD="yes"
+            ;;
+        -p|--capass)
+            CAPASS=$1
+            ;;
+        *)
+            NODE=$1
+         ;;
+    esac
+    shift
+done
+
+if [ -z "$CAPASS" ]; then
+    echo "CAPASS env variable not set"
+    exit 1
+fi
+
+
+if [ -z "$NODE" ]; then
+    echo "NODE argument missing"
+    echo "Usage:"
+    echo "fc-deploy-backstage-to-cloudnode.sh NODE"
+    echo "optional agument: -b|--build will run the build before deploy"
+    echo "optional argument: -p|--capass cloudadmin password. You can set it in env variables as well" 
+    exit 1
+fi
+
+cd ~/Documents/spotme/docker-devnode/backstage-app/
+
+if [ "$BUILD" == "yes" ]; then
+    docker-compose run bstg npm run gulp build    
+fi
+
+docker-compose run -e DB_URL=https://cloudadmin:$CAPASS@$NODE.4pax.com/backstage bstg ./tools/deploy.js
--- .bin/fc-shell.sh
+++ .bin/fc-shell.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-SHELL_NAME=\"*shell-`cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 8 | head -n 1`*\"
-
-emacsclient -c -n -e "(progn (shell $SHELL_NAME) (delete-other-windows))"
+SHELL_NAME=\"*term-`cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 8 | head -n 1`*\"
+ # 
+emacsclient -c -n -e '(progn (multi-term))'
