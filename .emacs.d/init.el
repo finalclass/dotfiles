9874cf97fe0a82b90ddddbc84c303adcc08dedbd
Szymon Wygnanski <s@finalclass.net>
2020-04-16 20:00:00 +0200

    Thu 16 Apr 2020 08:00:00 PM CEST

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -41,5 +41,6 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(default ((t (:inherit nil :extend nil :stipple nil :background "#2B2B2B" :foreground "#a9b7c6" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 70 :width normal :foundry "ADBO" :family "Source Code Pro"))))
  '(trailing-whitespace ((t (:foreground "#333333")))))
 (put 'narrow-to-region 'disabled nil)
